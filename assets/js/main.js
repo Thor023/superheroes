32c682cde8c11b7a4c6b6d33b0ba12479b34f0e9
PrgoramaThor <felipe.castaneda84@gmail.com>
2021-12-29 22:45:17 -0300

    arreglada busqueda y metodo de llenado de info

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,38 @@
 $(document).ready(function() {
-    let id = "70"
-    let direccion = "https://www.superheroapi.com/api/4564354093614559/"+id
-    $.ajax({
-        type:"GET",
-        url: direccion,
-        dataType:"json",
-        success: function(data) {
-            console.log(data);
-        },
-        error: function(error) {
-            console.log("Error")
-        },
-    });
+    $("button").on("click", function (){
+        let id = $("#formulario").val();
+        let direccion = `https://www.superheroapi.com/api/4564354093614559/${id}`;
+        $.ajax({
+            type:"GET",
+            url: direccion,
+            dataType:"json",
+            success: function(datosApi) {
+                console.log(datosApi, datosApi.image);;
+                console.log(datosApi.name, datosApi.connections, datosApi.work.occupation, datosApi.appearance.weight, datosApi.biography);
+                $('#resultado').append(`<h3> El Super encontrado es: </h3>`);
+                $("#avatar").html(`<img src="${datosApi.image.url}">`);
+                $(".nombre").html(datosApi.name);
+                $('#infosuper').append(`
+                Nombre: ${datosApi.name} <br>
+                Conexiones: ${datosApi.connections['group-affiliation']} <br>
+                Parientes: ${datosApi.connections.relatives}<br>
+                Ocupacion:${datosApi.work.occupation}<br>
+                Primera Aparición:${datosApi.biography['first-appearance']}<br>
+                Peso:${datosApi.appearance.weight[1]}<br>
+                Alias:${datosApi.biography.aliases}<br>
+                            
+                `);
+                $("#conexiones").html(datosApi.connections);
+                $("#ocupacion").html;
+                $("#aparicion").html(datosApi.biography);
+                $("#peso").html(datosApi.appearance);
+                $("#alianzas").html(datosApi.connections);
+                
+            },
+            error: function(error) {
+                    console.log("Error")
+            },
+        });
+        console.log(id)
+    })
 });
